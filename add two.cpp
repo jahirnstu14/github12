accddb403790ccba036e5cdf62ecb63d5c993ebb
jahirnstu14 <jahirkhan.ice.nstu.student14@gmail.com>
2022-08-17 11:23:20 +0600

    added needed file

---
+++ add two.cpp
@@ -0,0 +1,16 @@
+k,v#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a,b,c;
+cin>>a>>b;
+cout<<"my name is jahirul islam"<<endl;
+
+cout<<"addition of two number:"<<a*c*b<<endl;
+
+
+
+
+
+
+}
+
---
+++ divide.cpp
@@ -0,0 +1,2 @@
+include something new thing that is very important for us
+asfdsghjlk; asdfg hjkl; asdsfghjkjll
