9c776d514851917d6d83cf3d96b68d7a1d563284
jahirnstu14 <jahirkhan.ice.nstu.student14@gmail.com>
2022-08-17 11:35:53 +0600

    This is a new commit for what I originally planned to be an amendmend

--- add two.cpp
+++ add two.cpp
@@ -1,10 +1,11 @@
-k,v#include<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
 int a,b,c;
-cin>>a>>b;
+cin>>a>>b>>c;
 cout<<"my name is jahirul islam"<<endl;
-
+cout<<"do you know what is import please tell me in difference why and why not tell tell me"<<endl;
+cout<<a+b+c<<a*b*c<<a/b/c<<endl;
 cout<<"addition of two number:"<<a*c*b<<endl;
 
 
